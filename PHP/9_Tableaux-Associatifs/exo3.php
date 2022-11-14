8ecc0ec63995161036748b50993ef33dfb71e2c0
unknown <na.benyamina@outlook.fr>
2022-11-14 22:06:42 +0100

    14.11 home

--- PHP/9_Tableaux-Associatifs/exo3.php
+++ PHP/9_Tableaux-Associatifs/exo3.php
@@ -1,3 +1,21 @@
 <?php
 
-$a = array
+$tabNotes = array("Riri" => array(2, 10, 14), "Fifi" => array(10, 18, 12), "Loulou" => array(5, 9, 4),
+            "Mickey" => array(20, 16, 11), "Donald" => array(19, 1, 3), "Dingo" => array(15, 13, 7));
+
+foreach($tabNotes as $name => $notes){
+    echo "Nom : " . $name . "\n";
+    foreach($tabNotes[$name] as $notes){
+        echo $notes . "\n";
+    }
+}
+
+echo "\n";
+
+
+
+
+
+
+// $moyenne = array_sum($tabNotes)/count($tabNotes);
+// echo "La moyenne des notes des élèves est de : " . $moyenne;
